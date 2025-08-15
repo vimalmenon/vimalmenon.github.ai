d60d5976bb42d78844ac89e327783ba49da1a7a9
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-08-16 05:35:47 +0800

    feat: add Footer component with social links and update package dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import {Footer} from "../common"
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -36,6 +38,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} min-h-screen antialiased`}
       >
         {children}
+        <Footer />
       </body>
     </html>
   );
