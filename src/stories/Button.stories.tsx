a54936535c69b8b93f1d4905846f4d29a2915044
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-08-16 06:56:07 +0800

    feat: add Radix UI components and update imports in Button stories

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 
-import { Button } from '../components/ui/Button';
+import { Button } from '@components';
 import { Container } from "./Container.component"
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 
