05366f84607988101cd52dce8133de08eb9fe33e
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-08-16 06:46:54 +0800

    feat: implement Header component with navigation and theme controls, add Icons and Navigation data

---
+++ src/data/Icons/index.ts
@@ -0,0 +1,10 @@
+import { BookOpen, Home, Menu, Package, Settings } from 'lucide-react';
+
+
+export const Icons = {
+    BookOpen,
+    Home,
+    Menu,
+    Package,
+    Settings
+}
---
+++ src/data/Navigation/index.ts
@@ -0,0 +1,8 @@
+import { Icons } from "@data";
+
+export const Navigation = [
+  { href: "/", icon: Icons.Home, name: "Home" },
+  { href: "/blogs", icon: Icons.BookOpen, name: "Blogs" },
+  { href: "/release", icon: Icons.Package, name: "Release" },
+  { href: "/admin", icon: Icons.Settings, name: "Admin" },
+];
