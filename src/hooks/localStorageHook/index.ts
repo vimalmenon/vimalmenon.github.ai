d40f678c1c2c64234922e290e48c6879b9f7461d
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-08-16 21:53:38 +0800

    feat: implement localStorageHook to manage state with localStorage support

--- src/hooks/localStorageHook/index.ts
+++ src/hooks/localStorageHook/index.ts
@@ -1,3 +1,20 @@
 "use client";
 
-export const localStorageHook = () => {};
+import { useState } from "react";
+import { storageKey } from "@data";
+
+export const localStorageHook = () => {
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === "undefined") return null;
+    return JSON.parse(window.localStorage.getItem(storageKey) || "null");
+  });
+
+  const setValue = (value: any) => {
+    setStoredValue(value);
+    if (typeof window !== "undefined") {
+      window.localStorage.setItem(storageKey, JSON.stringify(value));
+    }
+  };
+
+  return [storedValue, setValue] as const;
+};
